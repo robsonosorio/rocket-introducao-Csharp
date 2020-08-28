2221e18d17f6638a59b0971c3a3d906a7f29f05f
Robson Osorio <robson.osorioo@gmail.com>
2020-08-28 13:43:01 -0300

    pratica oop 1

---
+++ modulo2_OOP_1/Entities/Carro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using treinamento.Entities.Enums;
+
+namespace treinamento
+{
+    public class Carro
+    {
+        public Cor Cor { get; set; }
+        public string Modelo { get; }
+        public int Portas { get; }
+
+        private bool ligado = false;
+
+        public bool Ligado
+        {
+            get
+            {
+                return ligado;
+            }
+        }
+
+        public Carro(Cor cor, string modelo, int portas)
+        {
+            Cor = cor;            
+            Modelo = modelo;
+            Portas = portas;
+        }
+
+        public string Ligar()
+        {
+            ligado = true;
+            return "O carro foi ligado !";
+        }
+        public string Desligar()
+        {
+            ligado = false;
+            return "O carro foi desligado !";
+        }
+        public string Andar()
+        {
+            return "O carro está andando !";
+        }
+    }
+}
---
+++ modulo2_OOP_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using treinamento.Entities.Enums;
+
+namespace treinamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Carro(Cor.Preto, "Honda", 5 );
+
+            Console.WriteLine("Carro de modelo " + c.Modelo + ", cor " + c.Cor + " e tem " + c.Portas);
+            Console.WriteLine("\n" + c.Ligar());
+            Console.WriteLine("\nCarro está ligado? " + c.Ligado);
+            Console.WriteLine("\n" + c.Andar());
+            Console.WriteLine("\n" + c.Desligar());
+            Console.WriteLine("\nCarro está desligado? " + c.Ligado);
+
+            c.Cor = Cor.Branco;
+            Console.WriteLine("\nNova cor do carro é " + c.Cor);
+        }
+    }
+}
