d5843772532fb92955297da00d1108db234dc481
Robson Osorio <robson.osorioo@gmail.com>
2020-08-30 14:58:11 -0300

    pratica oop 5

---
+++ ModuloCalculadora/OperacoesAritimeticas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuloCalculadora
+{
+    public static class OperacoesAritimeticas
+    {
+        public static double Adicao(double parcela1, double parcela2)
+        {
+            return parcela1 + parcela2;
+        }
+        public static double Subtracao(double minuando, double subtraindo)
+        {
+            return minuando - subtraindo;
+        }
+        public static double Multiplicacao(double multiplicando, double multiplicador)
+        {
+            return multiplicando * multiplicador;
+        }
+        public static double Divisao(double dividendo, double divisor)
+        {
+            return dividendo / divisor;
+        }
+    }
+}
